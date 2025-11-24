7fa054a4bcd0c0f55582dffcb3c8ed4543b2c6d4
Bruno Gil Ramirez <bruno.gilram@gmail.com>
2025-11-24 17:15:01 -0600

    feat: add User interface and implement user list with greeting functionality

--- ts/code/d1-foundation/from-scratch/src/index.ts
+++ ts/code/d1-foundation/from-scratch/src/index.ts
@@ -1,2 +1,34 @@
+import type { User } from "./User";
+
 const greeting: string = "Hello, TypeScript!";
 console.log(greeting);
+//day 1 exercise
+
+let users: User[] = [
+  { id: 1, name: "Alice", email: "alice@example.com", role: "admin" },
+  { id: 2, name: "Bob", email: "bob@example.com", role: "user" },
+  { id: 3, name: "Charlie", email: "charlie@example.com", role: "user" },
+];
+
+console.log("User List:");
+users.forEach((user) => {
+  console.log(
+    `ID: ${user.id}, Name: ${user.name}, Email: ${user.email}, Role: ${user.role}`
+  );
+});
+
+function greetUser(user: User): string {
+  return `Welcome, ${user.name}! You are logged in as a ${user.role}.`;
+}
+// "!" is used to assert that users[0] is not undefined
+let UserToGreet: User = users[0]!;
+console.log(greetUser(UserToGreet));
+
+let userwithoutId: Omit<User, "id"> = {
+  name: "Dave",
+  email: "dave@example.com",
+  role: "user",
+};
+// console.log(greetUser(userwithoutId )); // This would cause a TypeScript error because 'id' is missing
+console.log(greetUser({ ...userwithoutId, id: 4 }));
+//day 1 exercise end
