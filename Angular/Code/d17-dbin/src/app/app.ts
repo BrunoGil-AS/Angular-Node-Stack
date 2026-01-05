abcad887a860cd7f1025e83b9a319fa7649bdb37
Bruno Gil Ramirez <bruno.gilram@gmail.com>
2026-01-05 09:40:55 -0600

    feat: add main layout components including navbar, sidebar, content, and footer
    - Implemented Content component with basic structure and styles.
    - Created Navbar component with navigation links and styles.
    - Developed Sidebar component for menu options.
    - Added Footer component with copyright information.
    - Updated app component to include new layout components.
    - Added unit tests for all new components to ensure functionality.

--- Angular/Code/d17-dbin/src/app/app.ts
+++ Angular/Code/d17-dbin/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Content } from './components/content/content';
+import { Navbar } from './components/navbar/navbar';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Content, Navbar, Sidebar, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('d17-dbin');
